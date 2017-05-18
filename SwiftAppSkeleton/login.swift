1cca3032425bf99e345af7baccfe23b09cdc0e07
Duy Phuong <DuyPhuong@Duys-MacBook-Pro.local>
2017-05-18 17:44:44 +0700

    chỉnh sửa

---
+++ SwiftAppSkeleton/login.swift
@@ -0,0 +1,22 @@
+//
+//  login.swift
+//  SwiftAppSkeleton
+//
+//  Created by Duy Phuong on 5/18/17.
+//  Copyright © 2017 Techmaster. All rights reserved.
+//
+
+import UIKit
+
+class login: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+   
+
+   
+}
